f7205eeed5f814a05565d95e5efacc1d7ab8d597
Matt Petters <mcpetters@gmail.com>
2022-12-21 18:00:09 -0500

    Registers, FlagsRegister structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+struct Registers {
+    a: u8,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
+    f: u8,
+    h: u8,
+    l: u8,
+}
+
+struct FlagsRegister {
+    zero: bool,
+    subtract: bool,
+    half_carry: bool,
+    carry: bool,
+}
+
+impl Registers {
+    // some instructions write 2 bytes at a time so we need to read and write from these "virtual" 16 bit registers
+    fn get_bc(&self) -> u16 {
+        ((self.b as u16) << 8) | (self.c as u16)
+    }
+
+    fn set_bc(&mut self, value: u16) {
+        self.b = ((value & 0xFF00) >> 8) as u8;
+        self.c = (value & 0x00FF) as u8;
+    }
+}
 fn main() {
     println!("Hello, world!");
 }
